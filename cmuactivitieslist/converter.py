a7977ce32c095b75dcc5ebabe7bff7ec3d32be89
Dahoas <36314634+Dahoas@users.noreply.github.com>
2019-02-08 19:52:34 -0500

    Updated email retrieval

--- cmuactivitieslist/converter.py
+++ cmuactivitieslist/converter.py
@@ -11,7 +11,16 @@ result, data = mail.uid('search',None,"ALL")
 i = len(data[0].split())
 for x in range(i):
     latest_email_uid = data[0].split()[x]
-    result, email_data = mail.uit('fetch',latest_email_uid,'(RFC822)')
+    result, email_data = mail.uid('fetch',latest_email_uid,'(RFC822)')
     raw_email = email_data[0][1]
-    raw_email_string = raw_email.decdoe('utf-8')
-    print(raw_email_string)
+    raw_email_string = raw_email.decode('utf-8')
+    email_message = email.message_from_string(raw_email_string)
+    for part in email_message.walk():
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True)
+            save_string = str("Dumpgmailemail_" + str(x) + ".eml")
+            myfile = open(save_string, 'a+')
+            myfile.write(body.decode('utf-8'))
+            myfile.close()
+        else:
+            continue
